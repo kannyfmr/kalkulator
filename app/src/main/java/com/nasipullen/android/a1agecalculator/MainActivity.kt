4a23c08b4dc19f248330dd651ac4194d2021635c
kannyfmr <kannyf.mr@gmail.com>
2018-06-07 21:05:54 +0700

    final commit

--- app/src/main/java/com/nasipullen/android/a1agecalculator/MainActivity.kt
+++ app/src/main/java/com/nasipullen/android/a1agecalculator/MainActivity.kt
@@ -2,11 +2,22 @@ package com.nasipullen.android.a1agecalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvBirth.setText("")
     }
+
+    fun calculateAge(view: View){
+       val dob : String = eBirth.text.toString()
+       val currentYear =Calendar.getInstance().get(Calendar.YEAR)
+       val age = currentYear - dob.toInt()
+       tvBirth.setText("Umur Kamu Yaitu $age")
+   }
 }
